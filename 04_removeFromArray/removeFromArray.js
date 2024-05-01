29504268056c14eb1b7acc35e31eaaf13e8402a2
Lauren Murray <lauren_murray94@hotmail.com>
2024-05-01 10:25:44 +0100

    Simplifying removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,7 @@
-const removeFromArray = function (array, ...valuesToRemove) {
-    for (valueToRemove = 0; valueToRemove < valuesToRemove.length; valueToRemove++){
-        array = array.filter(function (valueToCheck) {
-            return valueToCheck !== valuesToRemove[valueToRemove];
-        });
-    }
-    return array;
+const removeFromArray = function (array, ...args) {
+    return array.filter(val => !args.includes(val))
 }
 
-removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
   
