c86c74d4309ec8d61c8b412cd8a0ab21ce7aa362
Lauren Murray <lauren_murray94@hotmail.com>
2024-05-01 10:16:15 +0100

    Completing remofeFromArray function to pass test cases - first attempt

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...valuesToRemove) {
+    for (valueToRemove = 0; valueToRemove < valuesToRemove.length; valueToRemove++){
+        array = array.filter(function (valueToCheck) {
+            return valueToCheck !== valuesToRemove[valueToRemove];
+        });
+    }
+    return array;
+}
 
-};
-
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
+  
