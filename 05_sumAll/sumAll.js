f1c459827a473ecd49dba09dd7c1dbbddfc732db
Lauren Murray <lauren_murray94@hotmail.com>
2024-05-01 11:10:22 +0100

    Completed sumAll function to pass test conditions - first attempt

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof(num1) != 'number' || typeof(num2) != 'number') return 'ERROR';
+    let finalSum = 0;
+    for (i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        finalSum +=i;
+    }
+    console.log(finalSum);
+    return finalSum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
